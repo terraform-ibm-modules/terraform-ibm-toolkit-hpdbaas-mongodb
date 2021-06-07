af36dc41e8ad38ef67b6e3373ea36ca97bc6bd43
Sean Sundberg <seansund@us.ibm.com>
2021-06-07 07:02:49 -0500

    Removes service auth and removes encryption config
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage1-service-auth.tf
+++
@@ -1,7 +0,0 @@
-module "service_authorization" {
-  source = "github.com/cloud-native-toolkit/terraform-ibm-iam-service-authorization.git"
-
-  source_service_name = "hyperp-dbaas-mongodb"
-  target_service_name = "hs-crypto"
-  roles = ["Reader"]
-}
